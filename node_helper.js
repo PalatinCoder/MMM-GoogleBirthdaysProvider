5f7834d51d5928d0ee6b2c86bc708287dd36d47a
Jan Syring-Lingenfelder <github@jan-sl.de>
2018-10-24 20:02:49 +0200

    feat: serve an iCal feed on the public url

--- node_helper.js
+++ node_helper.js
@@ -1,11 +1,16 @@
 var NodeHelper = require("node_helper")
+const icalGenerator = require("ical-generator")
+const ical = icalGenerator({name: 'MMM-GoogleBirthdaysProvider'})
 
 module.exports = NodeHelper.create({
     start: function() {
         console.log(this.name + ' helper is started')
+        this.expressApp.use("/" + this.name, (req, res) => {
+            ical.serve(res)
+        })
     },
     socketNotificationReceived: function(notification, payload) {
         console.log(this.name + " helper received socket notification: " + notification + " - Payload: " + payload)
         this.sendSocketNotification("ECHO", notification)
-    }
-})
+    },
+});
