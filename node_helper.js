345f80ad632f28e230779f30f379c9fb55de3d10
Jan Syring-Lingenfelder <github@jan-sl.de>
2018-10-24 23:38:09 +0200

    refactor(logging): log after the server is bound

--- node_helper.js
+++ node_helper.js
@@ -4,9 +4,11 @@ const ical = icalGenerator({name: 'MMM-GoogleBirthdaysProvider'})
 
 module.exports = NodeHelper.create({
     start: function() {
-        console.log(this.name + ' helper is started')
+
         this.expressApp.use("/" + this.name, (req, res) => {
             ical.serve(res)
         })
+
+        console.log(this.name + ' server is running')
     }
 });
