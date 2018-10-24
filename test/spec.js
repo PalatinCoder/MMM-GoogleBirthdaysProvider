8022aec36946fbe5355e783ca6a850874645d444
Jan Syring-Lingenfelder <github@jan-sl.de>
2018-10-24 20:01:45 +0200

    test: specify the public api
    which is an ical feed served over the given url

--- test/spec.js
+++ test/spec.js
@@ -1,3 +1,45 @@
+const NodeHelper = require("../node_helper");
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const express = require('express');
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
 describe('GoogleBirthdaysProvider', function() {
     it('should be tested');
+});
+
+describe('the server', function() {
+    var helper = new NodeHelper();
+    before('set up the express app', () => {
+        app = express();
+        helper.setName('MMM-GoogleBirthdaysProvider');
+        helper.setExpressApp(express());
+    });
+
+    this.beforeEach('start the helper', () => { helper.start() });
+    this.afterEach('stop the helper', () => { helper.stop() });
+
+    it('serves the correct url', done => {
+        chai.request(helper.expressApp)
+            .get('/MMM-GoogleBirthdaysProvider')
+            .end((err, res) => {
+                expect(res).to.have.status(200);
+                done();
+        });
+    });
+    it('serves the correct mime type and charset', done => {
+        chai.request(helper.expressApp)
+            .get('/MMM-GoogleBirthdaysProvider')
+            .end((err, res) => {
+                expect(res).to.have.header('content-type', 'text/calendar; charset=utf-8');
+                done();
+        });
+    });
+});
+
+describe('the generated ical feed', function() {
+    it('has the correct timezone');
+    it('contains all events');
 });
