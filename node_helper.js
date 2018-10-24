bfc24216da6df08b47c864f856cf7065a515cac6
Jan Syring-Lingenfelder <github@jan-sl.de>
2018-10-24 23:41:30 +0200

    feat(ical): add a dummy event

--- node_helper.js
+++ node_helper.js
@@ -5,6 +5,13 @@ const ical = icalGenerator({name: 'MMM-GoogleBirthdaysProvider'})
 module.exports = NodeHelper.create({
     start: function() {
 
+        ical.createEvent({
+            // dummy event, always 24h in the future
+            start: new Date(new Date().getTime() + 3600000*24),
+            summary: 'No Birthdays yet..',
+            allDay: true
+        })
+
         this.expressApp.use("/" + this.name, (req, res) => {
             ical.serve(res)
         })
