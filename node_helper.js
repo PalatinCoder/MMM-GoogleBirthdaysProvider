fd4054b000b7ffe6c595aaee0a73ce35bc6cf075
Jan Syring-Lingenfelder <github@jan-sl.de>
2018-12-09 15:44:40 +0100

    refactor: extract method and use moment.js to handle the date stuff

--- node_helper.js
+++ node_helper.js
@@ -1,34 +1,15 @@
 var NodeHelper = require("node_helper")
+const moment = require("moment")
 const icalGenerator = require("ical-generator")
-const ical = icalGenerator({name: 'MMM-GoogleBirthdaysProvider'})
 const apiHelper = require("./google-api-helper");
 
 module.exports = NodeHelper.create({
     start: function() {
 
-        apiHelper.getBirthdays(this.path)
-            .then(birthdays => {
-                birthdays.forEach(person => {
-                    // TODO: this need refactoring really really bad,
-                    // but I had to be quick by the time I implemented this :see_no_evil:
-                    var date = new Date();
-                    date.setUTCFullYear((new Date).getUTCFullYear());
-                    date.setUTCMonth(person.birthday.month - 1);
-                    date.setUTCDate(person.birthday.day);
-                    ical.createEvent({
-                        start: date,
-                        summary: `${person.name} hat Geburtstag`,
-                        allDay: true
-                    })
-                })
-            })
-            .catch(reason => {
-                // TODO: Show notification on the mirror ?
-                this._error(`API Helper error: ${reason.message}: ${reason.err || ''}`);
-            })
+        this._refreshData();
 
         this.expressApp.use("/" + this.name, (req, res) => {
-            ical.serve(res)
+            this.ical.serve(res)
         })
 
         this._log('Server is running')
@@ -38,7 +19,32 @@ module.exports = NodeHelper.create({
         this._log("Stopping helper");
     },
     
+    ical: icalGenerator({name: 'MMM-GoogleBirthdaysProvider', domain: 'mmm-googlebirthdaysprovider.local'}),
+    
+    _createIcalEvents: function(birthdays) {
+        birthdays.forEach(person => {
+            var date = moment({ day: person.birthday.day,
+                                month: person.birthday.month - 1,
+                                // year: person.birthday.year,
+                                hour: 0, minute: 0 , second: 0} );
+            this.ical.createEvent({
+                start: date,
+                summary: `${person.name} hat Geburtstag`,
+                allDay: true
+            });
+        });
+    },
 
+    _refreshData: function() {
+        this.ical.clear();
+        apiHelper.getBirthdays(this.path)
+            .then(birthdays => this._createIcalEvents(birthdays))
+            .catch(reason => {
+                // TODO: Show notification on the mirror ?
+                this._error(`API Helper error: ${reason.message}: ${reason.err || ''}`);
+            });
+    },
+    
     // custom logger utility to supress output in CI env
     _log: function(message) {
         if (process.env.NODE_ENV !== 'test') { console.log(`${this.name}: ${message}`) }
