13e696268dd4540e641901327523582c90a1dfa1
Jan Syring-Lingenfelder <github@jan-sl.de>
2019-01-13 16:43:41 +0100

    test: add tests for created and actually parsed events

--- test/spec.js
+++ test/spec.js
@@ -21,14 +21,11 @@ const chaiHttp = require('chai-http');
 const express = require('express');
 const expect = chai.expect;
 const ical = require('ical');
+const moment = require('moment');
 
 const REQUEST_URL = '/MMM-GoogleBirthdaysProvider';
 
 chai.use(chaiHttp);
-
-describe('GoogleBirthdaysProvider', function() {
-    it('should be tested');
-});
 
 describe('google-api-helper', () => {
     it('should be tested, shouldn\' it?');
@@ -74,4 +71,82 @@ describe('the public api', function() {
                 });
         });
     })
+});
+
+
+describe('ical events', () => {
+    describe('a created event', () => {
+        var helper = new NodeHelper();
+        helper._createIcalEvents([
+            { name: 'Johannes Testmann', birthday: { year: 1965, month: 09, day: 22 }},
+            { name: 'Elisabeth Testfrau', birthday: { month: 06, day: 13 }}
+        ]);
+
+        context('with a year set', () => {
+            var event = helper.ical.events()[0];
+            it('has a valid start date', () => expect(event.start()).to.be.an.instanceOf(moment, 'Start date is not a moment object'));
+            it('has the correct start date', () => {
+                expect(event.start().year()).to.equal(1965);
+                expect(event.start().month()+1).to.equal(09); // months are 0-indexed
+                expect(event.start().date()).to.equal(22);
+            });
+            it('has no end date', () => expect(event.end()).to.be.null);
+            it('is a full day event', () => expect(event.allDay()).to.be.true);
+            it('is repeating', () => expect(event.repeating()).to.be.have.property('freq', 'YEARLY'));
+        });
+        context('without a year set', () => {
+            var event = helper.ical.events()[1];
+            it('has a valid start date', () => expect(event.start()).to.be.an.instanceOf(moment, 'Start date is not a moment object'));
+            it('has the correct start date', () => {
+                expect(event.start().year()).to.equal(moment().year());
+                expect(event.start().month()+1).to.equal(06); // months are 0-indexed
+                expect(event.start().date()).to.equal(13);
+            });
+            it('has no end date', () => expect(event.end()).to.be.null);
+            it('is a full day event', () => expect(event.allDay()).to.be.true);
+            it('is not repeating', () => expect(event.repeating()).to.be.null);
+        });
+    });
+
+    describe('a parsed event', () => {
+        var helper = new NodeHelper();
+        helper.ical.clear();
+        helper._createIcalEvents([
+            { name: 'Elisabeth Testfrau', birthday: { month: 06, day: 13 }},
+            { name: 'Johannes Testmann', birthday: { year: 1965, month: 09, day: 22 }}
+        ]);
+        let icsString = helper.ical.toString();
+        let data = ical.parseICS(icsString);
+        let keys = [];
+
+        // Get the keys -> keys[0] will be without year and keys[1] will be the one with a year
+        for(var key in data) {
+            keys.push(key);
+        }
+
+        context('with a year set', () => {
+            let event = data[keys[1]];
+            it('has a start date', () =>  expect(event.start).to.be.an.instanceOf(Date));
+            it('has the correct start date', () => {
+                expect(event.start.getFullYear()).to.equal(1965);
+                expect(event.start.getMonth()+1).to.equal(09); // months are 0-indexed
+                expect(event.start.getDate()).to.equal(22);
+            });
+            it('has no end date', () => expect(event.end).to.be.undefined);
+            it('is a full day event');
+            it('is repeating', () => expect(event).to.have.property('rrule'));
+        });
+        context('without a year set', () => {
+            let event = data[keys[0]];
+            it('has a start date', () =>  expect(event.start).to.be.an.instanceOf(Date));
+            it('has the correct start date', () => {
+                expect(event.start.getFullYear()).to.equal(moment().year());
+                expect(event.start.getMonth()+1).to.equal(06); // months are 0-indexed
+                expect(event.start.getDate()).to.equal(13);
+            });
+            it('has no end date', () => expect(event.end).to.be.undefined);
+            it('is a full day event');
+            it('is not repeating', () => expect(event).to.not.have.property('rrule'));
+        });
+    });
 });
